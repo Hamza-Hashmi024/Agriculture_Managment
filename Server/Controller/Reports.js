63da18ded7e8b84ace48c6ca8321dcbdafbd8618
Hamza-Hashmi024 <hamza064@gmail.com>
2025-08-13 15:45:54 +0500

    Create Buyer Ledger Reports

--- Server/Controller/Reports.js
+++ Server/Controller/Reports.js
@@ -1,4 +1,4 @@
-const db = require('../config/db')
+const db = require("../config/db");
 
 const FarmerLedgerReports = (req, res) => {
   const { id } = req.params;
@@ -37,7 +37,40 @@ const FarmerLedgerReports = (req, res) => {
   });
 };
 
+const BuyerLedgerReports = (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT 
+    p.id AS payment_id,
+    p.date,
+    CASE 
+        WHEN p.payment_type = 'upfront' THEN 'Advance'
+        WHEN p.payment_type = 'later' THEN 'Payment'
+        WHEN p.payment_type = 'sale' THEN 'Sale/Lot'
+        ELSE 'Other'
+    END AS type,
+    CONCAT('#P', p.id) AS ref,
+    CASE WHEN p.payment_type = 'upfront' THEN p.amount ELSE 0 END AS debit,
+    CASE WHEN p.payment_type IN ('later','sale') THEN p.amount ELSE 0 END AS credit,
+    @balance := @balance + 
+        (CASE WHEN p.payment_type IN ('later','sale') 
+              THEN p.amount 
+              ELSE -p.amount END) AS balance,
+    p.notes
+FROM buyers b
+JOIN buyer_payments p ON p.buyer_id = b.id
+CROSS JOIN (SELECT @balance := 0) vars
+WHERE b.id = ?
+ORDER BY p.date;`;
+  db.query(query, [id], (err, result) => {
+    if (err) {
+      console.error("Error fetching farmer ledger:", err);
+      return res.status(500).json({ message: "Error fetching data" });
+    }
+    res.json(result);
+  });
+};
 
 module.exports = {
-    FarmerLedgerReports
-}
+  FarmerLedgerReports,
+   BuyerLedgerReports 
+};
--- Server/Routes/Reports_Router.js
+++ Server/Routes/Reports_Router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const asyncHandler = require('../MiddleWare/ErrorBoundry');
-const {FarmerLedgerReports} = require('../Controller/Reports')
+const {FarmerLedgerReports ,  BuyerLedgerReports } = require('../Controller/Reports')
 
 router.get('/reports/farmer/:id' , asyncHandler(FarmerLedgerReports));
+router.get('/buyer/report/:id' ,  asyncHandler(BuyerLedgerReports));
 
 module.exports = router;
